b8b50011fb7e86d1605915dc1d4f14c373c0754c
Carmen Albu <carmenalbu@Carmens-MacBook-Pro.local>
2017-02-10 09:25:50 +0200

    left only tachyons as css file for login and signup

--- application/views/login/login_view.php
+++ application/views/login/login_view.php
@@ -11,15 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href=<?php echo asset_url(). "css/style.css"; ?>>
     
-    <link rel="stylesheet" href=<?php echo asset_url()."css/dashboard-style.css"; ?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/addjob-style.css"; ?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/candidates-style.css"; ?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/jobs-style.css"; ?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/profile-style.css"; ?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/job-style.css";?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/apply-style.css";?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/responsive-style.css";?>>
-    <link rel="stylesheet" href=<?php echo asset_url()."css/login-style.css";?>>
+
     <link rel="stylesheet" href="https://unpkg.com/tachyons@4.6.1/css/tachyons.min.css"/>
 
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
