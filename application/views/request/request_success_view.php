3197725365a0563b4b21fe554bad4ae2329dda28
Jamie Gunson <jamie.gunson@googlemail.com>
2017-02-13 11:29:03 +0000

    Making some updates
    - Tweaking style of request_view.php using Tachyons.
    - Changing recipient email address to hello@nudj.co and tweaking
    message slightly.

--- application/views/request/request_success_view.php
+++ application/views/request/request_success_view.php
@@ -1,21 +1,24 @@
-
-
 <!-- CONTENT  -->
-
-    <div class="berightback">
-      <div class="cotent-back">
-        <div class="success">
-          <img src=<?php echo asset_url()."images/success.png";?> />
+  <div class="vh-100 dt w-100 avenir">
+  <div class="absolute w-100">
+    <nav class="dt w-100 mw8 center">
+        <div class="dtc w2 v-mid pa3">
+            <a href=<?php echo base_url(). ""; ?>  class="dib w3 grow-large">
+                <img src=<?php echo asset_url()."images/nudj-logo.png";?>></img>
+            </a>
         </div>
-        <p class="home-section-title">Success</p>
-        <p class="beback-subcaption">You've requested access</p>
-        <p class="success-description">We will get back to you shortly, until then follow us on Twitter for our latest roles.</p>
-      <div class="short-delimiter"></div>
+    </nav>
+    </div>
+    <div class="vh-100 dt w-100 black">
+      <div class="dtc v-mid ph3 ph6-l tc">
+        <iframe src="//giphy.com/embed/26ufmAlKt4ne2JDnq?html5=true" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <p class="fw6 f3">Nice one!</p>
+        <p class="f5 pb3 ml6-m mr6-m pr6-m pl6-m">Someone from our team will be in touch shortly. Until then do us a solid follow us on Twitter.</p>
       <div class="center-twitter">
         <iframe ng-show="user.twitter" allowtransparency="true" frameborder="0" scrolling="no"
               src="//platform.twitter.com/widgets/follow_button.html?screen_name=NudjHQ"
               style="width:208px; height:20px;"></iframe>
-
       </div>
       </div>
+    </div>
     </div>
